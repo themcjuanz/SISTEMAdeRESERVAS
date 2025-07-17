cc95f0540bbd40139bc0eff6827190a71380d1af
JuanJose <themc.juanz@gmail.com>
2025-07-17 08:15:54 -0500

    Cositas

--- MaletaBodega.py
+++ MaletaBodega.py
@@ -5,5 +5,5 @@ class MaletaBodega(Maleta):
     def __init__(self, peso = 0.0):
         super().__init__(tipo=tipoMaleta.BODEGA, peso=peso)
 
-    def calcularPrecio(self):
+    def calcularPrecio(self, tipo, peso):
         return 0.0
